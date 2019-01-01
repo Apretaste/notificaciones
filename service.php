8f645f4ba3986073fcd4dfb87d8b88b397d5223a
Ricardo Jimenez <ricardo@apretaste.com>
2019-01-01 18:36:17 -0500

    Porting to ejs templates

--- service.php
+++ service.php
@@ -16,15 +16,23 @@ class Notificaciones extends Service
 	{
 		// get the origins of the notifications if passed
 		$origins = [];
+
+		$servicesDir = \Phalcon\DI\FactoryDefault::getDefault()->get('path')['root'].'/services/';
 		if($request->query) foreach (explode(" ", $request->query) as $o) $origins[] = $o;
 
 		// get notifications
 		$notifications = Utils::getNotifications($request->userId, 20, $origins);
-
+		foreach($notifications as $notification){
+			$origin = $notification->origin;
+			$origin = strtolower($origin);
+			$notification->image = $servicesDir."$origin/$origin.png";
+			$notification->origin = substr_replace($origin,strtoupper(substr($origin,0,1)),0,1);
+			Utils::parseImgDir($notification->image);
+		}
 		// send response
 		$response = new Response();
 		$response->setResponseSubject("Sus notificaciones");
-		$response->createFromTemplate('basic.tpl', ['notificactions' => $notifications]);
+		$response->createFromTemplate('basic.ejs', ['notificactions' => $notifications]);
 		return $response;
 	}
 }
