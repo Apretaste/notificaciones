0160365559a41ea1fc872db3e9944ce1a7cef3f3
Salvi Pascual <salvi.pascual@gmail.com>
2020-08-13 14:58:18 -0400

    return the counter

--- scripts.js
+++ scripts.js
@@ -1,61 +1,35 @@
 // alerts counter
 var totalCountOfAlerts = 0;
 
-//
-// ON LOAD FUNCTIONS
-//
-
 $(document).ready(function(){
+	// get the notifications counter
 	totalCountOfAlerts = (typeof alerts=='undefined') ? 0 : alerts.length;
-	showScreensBasedOnCount();
 
 	// render tabs
 	$('.tabs').tabs();
 });
 
-//
-// FUCTIONS FOR THE SERVICE
-//
-
 // send the notificationd to be deleted
 function deleteAlert(id) {
-	// delete from the backend
+	// if is the last notification, update the app counter 
+	if(totalCountOfAlerts <= 1 ) {
+		deleteAllAlerts();
+		return;
+	}
+
+	// else delete notification from the backend
 	apretaste.send({
 		command: 'NOTIFICACIONES LEER',
 		data: {id: id},
 		redirect: false
 	});
 
-	// delete from screen and decrease counter
+	// and from the view and decrease counter
 	$('#'+id).hide();
 	totalCountOfAlerts--;
-
-	// show message if all alerts were deleted
-	showScreensBasedOnCount();
 }
 
-// send the notificationd to be deleted
+// clean all the notifications
 function deleteAllAlerts() {
-	// delete from the backend
-	apretaste.send({
-		command: 'NOTIFICACIONES BORRAR',
-		redirect: false
-	});
-
-	// decrease counter
-	totalCountOfAlerts = 0;
-
-	// show no message message 
-	showScreensBasedOnCount();
-}
-
-// display the alerts or the message based on the count
-function showScreensBasedOnCount() {
-	if(totalCountOfAlerts <= 0 ) {
-		$('#no-notes').show();
-		$('#yes-notes').hide();
-	} else {
-		$('#no-notes').hide();
-		$('#yes-notes').show();
-	}
+	apretaste.send({command: 'NOTIFICACIONES BORRAR'});
 }
